7924f845a36a18d40a5dc1860e0ed2d75e576f61
Covalsqy <gabimbergmann4@gmail.com>
2024-10-23 10:32:56 -0300

    Refinar testes de adicionar funcionários

--- src/main.py
+++ src/main.py
@@ -10,11 +10,19 @@ class Empresa:
         return self.lista_funcionarios
 
     def adicionar_funcionario(self, f):
-        self.lista_funcionarios.append(f)
+        if f not in self.lista_funcionarios:
+            f.id = len(self.lista_funcionarios) + 1
+            self.lista_funcionarios.append(f)
+        else:
+            raise Exception("Funcionário já está na empresa")
 
 class Funcionario:
     def __init__(self, name):
         self.nome = name
+        self.id = 0
 
     def get_nome(self):
         return self.nome
+
+    def get_id(self):
+        return self.id
--- tests/tests_gerenciador_ocorrencias.py
+++ tests/tests_gerenciador_ocorrencias.py
@@ -16,7 +16,33 @@ class MyTestCase(unittest.TestCase):
         empresa = Empresa("W")
         funcionario = Funcionario("João")
         empresa.adicionar_funcionario(funcionario)
-        self.assertEqual(empresa.get_funcionarios()[0].nome, funcionario.get_nome())
+        self.assertEqual(empresa.get_funcionarios()[0].get_nome(), funcionario.get_nome())
+
+    def test_associar_dois_funcionarios(self):
+        empresa = Empresa("W")
+        funcionario1 = Funcionario("José")
+        funcionario2 = Funcionario("Marcos")
+        empresa.adicionar_funcionario(funcionario1)
+        empresa.adicionar_funcionario(funcionario2)
+        self.assertEqual(empresa.get_funcionarios()[0].get_nome(), funcionario1.get_nome())
+        self.assertEqual(empresa.get_funcionarios()[1].get_nome(), funcionario2.get_nome())
+
+    def test_associar_dois_funcionarios_mesmo_nome(self):
+        empresa = Empresa("W")
+        funcionario1 = Funcionario("Marcos")
+        funcionario2 = Funcionario("Marcos")
+        empresa.adicionar_funcionario(funcionario1)
+        empresa.adicionar_funcionario(funcionario2)
+        self.assertEqual(empresa.get_funcionarios()[0].get_id(), funcionario1.get_id())
+        self.assertEqual(empresa.get_funcionarios()[1].get_id(), funcionario2.get_id())
+        self.assertEqual(empresa.get_funcionarios()[0].get_nome(), empresa.get_funcionarios()[1].get_nome())
+        self.assertNotEqual(empresa.get_funcionarios()[0].get_id(), empresa.get_funcionarios()[1].get_id())
+
+    def test_associar_funcionario_ja_associado_a_empresa(self):
+        empresa = Empresa("W")
+        funcionario = Funcionario("Jorge")
+        empresa.adicionar_funcionario(funcionario)
+        self.assertRaises(Exception, empresa.adicionar_funcionario, funcionario)
 
 if __name__ == '__main__':
     unittest.main()
