7067597143835708ee0d69818fae77985eed58a5
Covalsqy <gabimbergmann4@gmail.com>
2024-10-23 10:06:09 -0300

    Teste Criação Funcionário

--- src/main.py
+++ src/main.py
@@ -1,3 +1,7 @@
 class Empresa:
     def __init__(self, name):
         self.nome = name
+
+class Funcionario:
+    def __init__(self, name):
+        self.nome = name
--- tests/tests_gerenciador_ocorrencias.py
+++ tests/tests_gerenciador_ocorrencias.py
@@ -1,11 +1,16 @@
-from src.main import Empresa
+from src.main import Empresa, Funcionario
 import unittest
 
 class MyTestCase(unittest.TestCase):
     def test_criar_empresa_w(self):
         empresa = Empresa("W")
-        self.assertIsNotNone(empresa)  # add assertion here
+        self.assertIsNotNone(empresa)
         self.assertEqual(empresa.nome, "W")
+
+    def test_criar_funcionario(self):
+        funcionario = Funcionario("Arthur")
+        self.assertIsNotNone(funcionario)
+        self.assertEqual(funcionario.nome, "Arthur")
 
 
 if __name__ == '__main__':
